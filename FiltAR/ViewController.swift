dc6670e15aa34082940b0043db4e701261e2eac1
egorhristoforov <egor.hristoforov@mail.ru>
2019-08-17 12:07:25 +0300

    Update remote

--- FiltAR/ViewController.swift
+++ FiltAR/ViewController.swift
@@ -22,11 +22,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.debugOptions = [.showWorldOrigin]
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
-        // Set the scene to the view
+        let boxNode = createBox(boxWidth: 0.18, boxHeight: 0.28, boxLength: 0.01)
+        boxNode.position = SCNVector3(0, 0, -0.5)
+        let billboardConstraint = SCNBillboardConstraint()
+        billboardConstraint.freeAxes = [SCNBillboardAxis.Y, SCNBillboardAxis.X]
+        boxNode.constraints = [billboardConstraint]
+        scene.rootNode.addChildNode(boxNode)
         sceneView.scene = scene
     }
     
@@ -36,6 +41,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        configuration.worldAlignment = .gravity
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -47,6 +54,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if let camera = sceneView.pointOfView {
+            let nodes = sceneView.scene.rootNode.childNodes
+            guard !nodes.isEmpty else {return}
+            for node in nodes {
+                //node.simdScale = simd_float3(0.5, 0.5, 0.5)
+                //node.position = SCNVector3(camera.position.x, camera.position.y, camera.position.z - 0.5)
+            }
+        }
+    }
+    
     // MARK: - ARSCNViewDelegate
     
 /*
@@ -57,6 +75,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    
+    /*override func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        let pov = sceneView.pointOfView
+        let position = pov?.position
+        
+        let x = position?.x
+        let y = position?.y
+        let z = position?.z
+        
+        
+    }*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -71,5 +100,56 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    func createBox(boxWidth width: CGFloat, boxHeight height: CGFloat, boxLength length: CGFloat, boxRadius radius: CGFloat = 0) -> SCNNode {
+        
+        let borderBox = SCNBox(width: width + 0.01, height: height + 0.01, length: length + 0.01, chamferRadius: radius)
+        
+        let borderMaterial = SCNMaterial()
+        borderMaterial.diffuse.contents = UIColor(cgColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        
+        borderBox.materials = [borderMaterial]
+        
+        let box = SCNBox(width: width, height: height, length: length, chamferRadius: radius)
+        
+        let frontSideMaterial = SCNMaterial()
+        frontSideMaterial.diffuse.contents = UIImage(named: "SteveJobs")
+        
+        let rightSideMaterial = SCNMaterial()
+        rightSideMaterial.diffuse.contents = UIColor.white
+        
+        let backSideMaterial = SCNMaterial()
+        backSideMaterial.diffuse.contents = UIColor.white
+        
+        let leftSideMaterial = SCNMaterial()
+        leftSideMaterial.diffuse.contents = UIColor.white
+        
+        let topSideMaterial = SCNMaterial()
+        topSideMaterial.diffuse.contents = UIColor.white
+        
+        let bottomSideMaterial = SCNMaterial()
+        bottomSideMaterial.diffuse.contents = UIColor.white
+        
+        box.materials = [frontSideMaterial, rightSideMaterial, backSideMaterial, leftSideMaterial, topSideMaterial, bottomSideMaterial]
+        
+        let resultNode = SCNNode()
+        let imageNode = SCNNode(geometry: box)
+        let borderNode = SCNNode(geometry: borderBox)
+        
+        borderNode.position = SCNVector3(0, 0, 0)
+        imageNode.position = SCNVector3(0, 0, 0.0055)
+        
+        resultNode.addChildNode(borderNode)
+        resultNode.addChildNode(imageNode)
+        
+        let moveDown = SCNAction.move(by: SCNVector3(0, -0.025, 0), duration: 1)
+        let moveUp = SCNAction.move(by: SCNVector3(0,0.025,0), duration: 1)
+        let waitAction = SCNAction.wait(duration: 0.1)
+        let hoverSequence = SCNAction.sequence([moveUp,waitAction,moveDown, waitAction])
+        let loopSequence = SCNAction.repeatForever(hoverSequence)
+        resultNode.runAction(loopSequence)
+     
+        return resultNode
     }
 }
